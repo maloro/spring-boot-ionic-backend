4517af2a5b9b5b41d362eca1ee051e70f1fb2340
Marcelo A Loro <marcelo.a.loro@gmail.com>
2021-07-15 11:28:53 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/udemy/cursomc/domain/ItemPedido.java
+++ src/main/java/com/udemy/cursomc/domain/ItemPedido.java
@@ -6,10 +6,13 @@ import java.util.Objects;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 
@@ -31,6 +34,7 @@ public class ItemPedido implements Serializable{
 		
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
